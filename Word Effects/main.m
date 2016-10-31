7f918665e483478a20dbbebf3bcb12a7a53c3cfc
stef <stef.verve@gmail.com>
2016-10-31 17:00:51 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Stefan Verveniotis on 2016-10-31.
+//  Copyright © 2016 Stefan Verveniotis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
